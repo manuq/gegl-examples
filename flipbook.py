ecdadc062992b5fe066c010152c599d20cb886f2
Manuel Quiñones <manuel.por.aca@gmail.com>
2014-03-24 19:37:59 -0300

    Py example, add mypaint surface with gegl backend
    - load charcoal brush with mypaint lib
    - capture input events and draw on the surface

--- flipbook.py
+++ flipbook.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python
 
+import sys
+sys.path.append("../mypaint")
+
 from gi.repository import Gegl, Gtk
 from gi.repository import GeglGtk3 as GeglGtk
+
+from lib import tiledsurface, brush
 
 
 class FlipbookApp(object):
     def __init__(self):
+        brush_file = open('../mypaint/brushes/classic/charcoal.myb')
+        brush_info = brush.BrushInfo(brush_file.read())
+        brush_info.set_color_rgb((0.0, 0.0, 0.0))
+        self.brush = brush.Brush(brush_info)
+
+        self.button_pressed = False
+        self.last_event = (0.0, 0.0, 0.0)  # (x, y, time)
+
+        self.surface = tiledsurface.GeglSurface()
+        self.surface_node = self.surface.get_node()
+
         self.graph = None
         self.nodes = {}
 
@@ -26,6 +42,9 @@ class FlipbookApp(object):
         self.nodes['background'].connect_to(
             "output", self.nodes['root'], "input")
 
+        self.surface_node.connect_to(
+            "output", self.nodes['root'], "aux")
+
     def init_ui(self):
         window = Gtk.Window()
         window.props.title = "Flipbook"
@@ -35,11 +54,17 @@ class FlipbookApp(object):
         top_box.props.orientation = Gtk.Orientation.VERTICAL;
         window.add(top_box)
 
+        event_box = Gtk.EventBox()
+        event_box.connect("motion-notify-event", self.motion_to_cb)
+        event_box.connect("button-press-event", self.button_press_cb)
+        event_box.connect("button-release-event", self.button_release_cb)
+        top_box.add(event_box)
+
         view_widget = GeglGtk.View()
         view_widget.set_node(self.nodes['root'])
         view_widget.set_autoscale_policy(GeglGtk.ViewAutoscale.DISABLED)
         view_widget.set_size_request(800, 400)
-        top_box.add(view_widget)
+        event_box.add(view_widget)
 
         window.show_all()
 
@@ -48,6 +73,26 @@ class FlipbookApp(object):
 
     def destroy_cb(self, *ignored):
         Gtk.main_quit()
+
+    def motion_to_cb(self, widget, event):
+        (x, y, time) = event.x, event.y, event.time
+
+        pressure = 0.5
+        dtime = (time - self.last_event[2])/1000.0
+        if self.button_pressed:
+            self.surface.begin_atomic()
+            self.brush.stroke_to(self.surface.backend, x, y, pressure,
+                                 0.0, 0.0, dtime)
+            self.surface.end_atomic()
+
+        self.last_event = (x, y, time)
+
+    def button_press_cb(self, widget, event):
+        self.button_pressed = True
+
+    def button_release_cb(self, widget, event):
+        self.button_pressed = False
+        self.brush.reset()
 
 
 if __name__ == '__main__':
